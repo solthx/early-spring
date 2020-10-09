d0cdd04296a6fd6815ed3aee5dcb86b7722b3e32
清雪冰橙 <1154238323@qq.com>
2020-10-10 00:01:21 +0800

    TEST

---
+++ early-spring-example/src/main/java/com/earlyspring/example/config/ControllerConfig.java
@@ -0,0 +1,12 @@
+package com.earlyspring.example.config;
+
+import com.earlyspring.ioc.bean.annotation.Component;
+import com.earlyspring.ioc.bean.annotation.ComponentScan;
+
+/**
+ * @author czf
+ * @Date 2020/10/8 6:00 下午
+ */
+@ComponentScan("com.earlyspring.example")
+public class ControllerConfig {
+}
---
+++ early-spring-example/src/main/java/com/earlyspring/example/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.earlyspring.example.controller;
+
+import com.earlyspring.ioc.bean.annotation.Controller;
+import com.earlyspring.webmvc.annotation.AsResponse;
+import com.earlyspring.webmvc.annotation.RequestEntrance;
+import com.earlyspring.webmvc.annotation.RequestParam;
+
+/**
+ * @author czf
+ * @Date 2020/10/4 10:21 下午
+ */
+@Controller
+@RequestEntrance(pattern = "/demo")
+public class TestController {
+
+    @RequestEntrance(pattern = "czf")
+    @AsResponse
+    public String hello(@RequestParam("name") String abcd){
+        return "hi, " + abcd;
+    }
+}
